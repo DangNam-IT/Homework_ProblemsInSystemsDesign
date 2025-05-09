eccd30297e78e878997613b8282c5c22efd8b0c1
Dang Phuong Nam <Dang Phuong Nam@DESKTOP-C5E8ROK>
2025-05-09 17:23:42 +0700

    make folder, file and classes

---
+++ 36_2251172431_DangPhuongNam_BaiTap2/src/main/java/com/mycompany/ht12/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ht12;
+
+/**
+ *
+ * @author Dang Phuong Nam
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
